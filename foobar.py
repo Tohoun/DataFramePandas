824aadefdb2a3b08aa144d4925db4bce45c85975
Romeo tohoun <romeotohoun@gmail.com>
2022-02-09 20:06:31 +0000

    Definition de la methode __mul__ dans la classe DataSeries

--- foobar.py
+++ foobar.py
@@ -1,3 +1,4 @@
+
 import typing as t
 
 
@@ -10,6 +11,8 @@ class DitSeries:
         - le contenu de la colonne sous forme de Dictionnaire
         - La clé du Dictionnaire est un entier
         - La valeur du Dictionnaire est de n'importe quel type"""
+        self.name = name
+        self.data = data
 
     def __mul__(self, other: int) -> "DitSeries":
         """ Implementation de l'opération de multiplication.
@@ -17,6 +20,10 @@ class DitSeries:
         - La valeur renvoyée est de type DitSeries
         - Chaque élément de la série est multiplié par l'argument
         """
+        new_objet = self.data
+        for cle in self.data.keys():
+            new_objet.update({cle:other*self.data[cle]})       
+        return DitSeries(self.name, new_objet)
 
     def __div__(self, other: int) -> "DitSeries":
         """ Implementation de l'opération de division.
@@ -43,41 +50,41 @@ class DitSeries:
         return f"<DitSeries: {self.name} {self.data}>"
 
 
-class DitDataFrame:
-    """ """
-    def __init__(self, d: t.Dict[str, t.List[t.Any]]):
-        """ Constructeur prenant un seul parametre
-        - un dictionnaire
-        - la clé du dictionnaire est une chaine de caractère
-        - la valeur du dictionnaire est une liste contenant des elements de n'importe quel type
+# class DitDataFrame:
+#     """ """
+#     def __init__(self, d: t.Dict[str, t.List[t.Any]]):
+#         """ Constructeur prenant un seul parametre
+#         - un dictionnaire
+#         - la clé du dictionnaire est une chaine de caractère
+#         - la valeur du dictionnaire est une liste contenant des elements de n'importe quel type
 
-        - les attributs initialisés seront:
-        - self.series_map, correspondra à un dictionnaire de type DitSeries
-        - self.length, la longueur du DataFrame
-        """
+#         - les attributs initialisés seront:
+#         - self.series_map, correspondra à un dictionnaire de type DitSeries
+#         - self.length, la longueur du DataFrame
+#         """
 
 
-    def __getitem__(self, key: str) -> DitSeries:
-        return self.series_map[key]
+#     def __getitem__(self, key: str) -> DitSeries:
+#         return self.series_map[key]
 
-    def __setitem__(self, key: str, value: DitSeries) -> None:
-        if key not in self.series_map:
-            self.series_map[key] = DitSeries(key, {})
-        for i, v in value.data.items():
-            self[key].data[i] = v
+#     def __setitem__(self, key: str, value: DitSeries) -> None:
+#         if key not in self.series_map:
+#             self.series_map[key] = DitSeries(key, {})
+#         for i, v in value.data.items():
+#             self[key].data[i] = v
 
-    def __repr__(self):
-        width = 5
-        headers = " | ".join(header.rjust(width) for header in self.series_map)
-        divider = "-" * len(headers)
-        rows = tuple(
-            " | ".join(
-                str(self.series_map[k].data.get(i, "NaN")).rjust(width)
-                for k in self.series_map
-            )
-            for i in range(self.length)
-        )
-        return "\n".join((headers, divider) + rows) + "\n"
+#     def __repr__(self):
+#         width = 5
+#         headers = " | ".join(header.rjust(width) for header in self.series_map)
+#         divider = "-" * len(headers)
+#         rows = tuple(
+#             " | ".join(
+#                 str(self.series_map[k].data.get(i, "NaN")).rjust(width)
+#                 for k in self.series_map
+#             )
+#             for i in range(self.length)
+#         )
+#         return "\n".join((headers, divider) + rows) + "\n"
 
 
 if __name__ == "__main__":
@@ -93,15 +100,15 @@ if __name__ == "__main__":
     ds4 = ds3 > 95
     print(ds4)   # <DitSeries: serie {0: False, 1: True, 2: True}>
 
-    df = DitDataFrame(
-        {
-            "id": [18, 23, 11],
-            "names": ["foo", "bar", "baz"],
-            "weights": [42.3, 57.6, 89.1],
-            "heights": [160, 192, 173],
-        }
-    )
-    print(df)
+    # df = DitDataFrame(
+    #     {
+    #         "id": [18, 23, 11],
+    #         "names": ["foo", "bar", "baz"],
+    #         "weights": [42.3, 57.6, 89.1],
+    #         "heights": [160, 192, 173],
+    #     }
+    # )
+    # print(df)
 # affiche le tableau suivant
 
 #       id | names | weights | heights
@@ -110,10 +117,10 @@ if __name__ == "__main__":
 #       23 |   bar |  57.6 |   192
 #       11 |   baz |  89.1 |   173
 
-    print(df.length)  # affiche 3
+    # print(df.length)  # affiche 3
 
-    names = df["names"]
-    print(names)  # <DitSeries: names {0: 'foo', 1: 'bar', 2: 'baz'}>
+    # names = df["names"]
+    # print(names)  # <DitSeries: names {0: 'foo', 1: 'bar', 2: 'baz'}>
 
-    where = df["heights"] > 160
-    print(df["names"][where])  # <DitSeries: names {1: 'bar', 2: 'baz'}>
+    # where = df["heights"] > 160
+    # print(df["names"][where])  # <DitSeries: names {1: 'bar', 2: 'baz'}>
